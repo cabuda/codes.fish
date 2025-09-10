35c26b17172b7e2ce630e993945794f6befab5d5
cabuda <cabuda@outlook.com>
2025-09-10 16:05:38 +0800

    Update proxy.fish

--- conf.d/proxy.fish
+++ conf.d/proxy.fish
@@ -1,22 +1,24 @@
-function proxy_ssh_on
-    # 设置代理（修改成你的代理地址）
+function proxy_on
     set -Ux http_proxy "socks5h://localhost:1080"
     set -Ux https_proxy "socks5h://localhost:1080"
     set -Ux all_proxy "socks5h://localhost:1080"
 end
 
-function proxy_ssh_off
-    # 执行完后取消代理
+function proxy_off
     set -Ue http_proxy
     set -Ue https_proxy
     set -Ue all_proxy
+
+    set -U http_proxy
+    set -U https_proxy
+    set -U all_proxy
 end
 
-function proxy_ssh
-    proxy_ssh_on
+function proxy
+    proxy_on
 
     # 执行传入的命令
     command $argv
 
-    proxy_ssh_off
+    proxy_off
 end
